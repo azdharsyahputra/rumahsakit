fd1cf631be1a79ab1af0cd0b1fa96723a77e414b
azdharsyahputra <if23.muhammadsyahputra@mhs.ubpkarawang.ac.id>
2025-06-20 13:35:13 +0700

    qr

--- src/app/pendaftaran/status-antrian/status-antrian.module.ts
+++ src/app/pendaftaran/status-antrian/status-antrian.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
+import { QRCodeComponent } from 'angularx-qrcode';
 
 import { StatusAntrianPageRoutingModule } from './status-antrian-routing.module';
 
@@ -13,7 +14,8 @@ import { StatusAntrianPage } from './status-antrian.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    StatusAntrianPageRoutingModule
+    StatusAntrianPageRoutingModule,
+    QRCodeComponent
   ],
   declarations: [StatusAntrianPage]
 })
